28ccf7b3d644804302b1982750d0026cb129368b
Sebastian77784 <sebastiannn231@gmail.com>
2025-08-27 16:25:26 -0600

    Modificación a la BD
    Creación a la tabla post

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -8,3 +8,12 @@ Route::get('/', function () {
 Route::get('/contacto',function(){
     return view('contacto');
 });
+Route::get('/post',function(){
+    return view('post');
+});
+Route::get('/index',function(){
+    return view('index');
+});
+Route::get('/about',function(){
+    return view('about');
+});
